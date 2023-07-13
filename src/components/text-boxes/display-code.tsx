29e5bb23f8db469016db3637b052fe95933d5f49
MRDGH2821 <mihirrabade@gmail.com>
2023-07-13 20:53:28 +0530

    fix: move text area value child into property

--- src/components/text-boxes/display-code.tsx
+++ src/components/text-boxes/display-code.tsx
@@ -2,13 +2,8 @@ import { component$, useStyles$ } from '@builder.io/qwik';
 import textBoxStyles from './text_box.css?inline';
 export default component$((props: { code: string | object }) => {
   useStyles$(textBoxStyles);
-  const finalCode = typeof props.code === 'object' ? JSON.stringify(props.code, null, 2) : props.code;
+  const finalCode =
+    typeof props.code === 'object' ? JSON.stringify(props.code, null, 2) : props.code;
 
-  return (
-    <>
-      <textarea readOnly={true} cols={50} rows={10}>
-        {finalCode}
-      </textarea>
-    </>
-  );
+  return <textarea value={finalCode} readOnly={true} cols={50} rows={10} />;
 });
